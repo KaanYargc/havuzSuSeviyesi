f36d20b92e2fb690b4e95da2c304618ad2f8f815
OnurKonuk174 <51912619+OnurKonuk174@users.noreply.github.com>
2022-10-05 19:05:36 +0300

    Initial commit

---
+++ src/havuzSuSeviyesi1/Main.java
@@ -0,0 +1,41 @@
+package havuzSuSeviyesi1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double suSeviyesi = 77;
+		if (suSeviyesi > 0 && suSeviyesi < 10) {
+			System.out.println("||");
+		}
+		if (suSeviyesi > 10 && suSeviyesi < 20) {
+			System.out.println("||||");
+		}
+		if (suSeviyesi > 20 && suSeviyesi < 30) {
+			System.out.println("||||||");
+		}
+		if (suSeviyesi > 30 && suSeviyesi < 40) {
+			System.out.println("||||||||");
+
+		}
+		if (suSeviyesi > 40 && suSeviyesi < 50) {
+			System.out.println("||||||||||");
+		}
+		if (suSeviyesi > 50 && suSeviyesi < 60) {
+			System.out.println("||||||||||");
+		}
+		if (suSeviyesi > 60 && suSeviyesi < 70) {
+			System.out.println("||||||||||");
+		}
+		if (suSeviyesi > 70 && suSeviyesi < 80) {
+		}
+		System.out.println("||||||||||||");
+		if (suSeviyesi > 80 && suSeviyesi < 90) {
+			System.out.println("||||||||||||");
+		}
+		if (suSeviyesi > 90 && suSeviyesi < 100) {
+			System.out.println("|||||||||||||||");
+		}
+
+	}
+
+}
